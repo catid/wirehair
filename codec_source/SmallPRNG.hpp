3cc292b9714f5457839287bad3c797dfbd8c6f41
Christopher A Taylor <mrcatid@gmail.com>
2012-02-28 01:26:55 -0500

    Update license text

--- codec_source/SmallPRNG.hpp
+++ codec_source/SmallPRNG.hpp
@@ -2,7 +2,7 @@
 	Copyright (c) 2012 Christopher A. Taylor.  All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are :
+	modification, are permitted provided that the following conditions are met:
 
 	* Redistributions of source code must retain the above copyright notice,
 	  this list of conditions and the following disclaimer.
