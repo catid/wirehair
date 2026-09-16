68196dd183921a5c77735e991e17a959e5800766
catid <catid@users.noreply.github.com>
2026-09-16 05:49:48 +0000

    bench(wh2): advance final cost claim to R24 namespace

--- bench/Wh2K4SerializedCostR1.cpp
+++ bench/Wh2K4SerializedCostR1.cpp
@@ -2,5 +2,5 @@
 #define WH2_SMALL_COST_K 4
 #define WH2_SMALL_COST_REPAIRS 8
 #define WH2_SMALL_COST_PROTOCOL "wirehair.wh2.k4-serialized-cost-r0"
-#define WH2_SMALL_COST_CLAIM_PATH "/var/tmp/wh2-k4-serialized-cost-r1.R23/science/CLAIM.json"
+#define WH2_SMALL_COST_CLAIM_PATH "/var/tmp/wh2-k4-serialized-cost-r1.R24/science/CLAIM.json"
 #include "Wh2SmallLifecycleWorkerR0.h"
